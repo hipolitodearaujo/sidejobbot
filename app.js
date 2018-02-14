c52d9b9ed8c469532cf1df06ac07fd7d32e94af4
Denis Hipólito de Araujo <hipolitodearaujo@outlook.com>
2018-02-13 23:06:37 -0400

    alterado para texto em português

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ bot.set('storage', tableStorage);
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+        builder.Prompts.text(session, "Oi... Qual o seu nome?");
     },
     function (session, results) {
         session.userData.name = results.response;
