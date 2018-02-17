bef0234387956be9cceb478d4c0a6783e55693f0
Denis Hipólito de Araujo <hipolitodearaujo@outlook.com>
2018-02-17 16:32:41 -0400

    .

--- app.js
+++ app.js
@@ -7,16 +7,10 @@ var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
 
 //Create chat connector for communicating with the Bot Framework Service
-//var connector = new builder.ChatConnector({
-//    appId: process.env.MicrosoftAppId,
-//    appPassword: process.env.MicrosoftAppPassword,
-//    openIdMetadata: process.env.BotOpenIdMetadata 
-//});
-
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID ? process.env.MICROSOFT_APP_ID : '',
-    appPassword: process.env.MICROSOFT_APP_PASSWORD ? process.env.MICROSOFT_APP_PASSWORD : '',
-   gzipData: true
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
 //Create your bot with a function to receive messages from the user
